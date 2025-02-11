e2c45b6728e82f449458ca8ac6e66979bd861310
zaki <hanesmitter3@gmail.com>
2025-02-11 17:06:59 +0300

    Custom header safety area

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,10 +1,18 @@
 import { useNavigation, useLocalSearchParams } from "expo-router";
 import { View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigationState } from "@react-navigation/native";
+import { IconSymbol } from "./ui/IconSymbol";
 
 export default function CustomHeader({ title }: { title: string }) {
   const navigation = useNavigation();
   const params = useLocalSearchParams();
+
+  // Get the navigation state to check the history stack
+  const navigationState = useNavigationState((state) => state);
+
+  // Check if there is more than one route in the history stack
+  const canGoBack = navigationState.routes.length > 1;
 
   // Check if the navigation is coming from the 404 screen
   const isFrom404 = params.from404 === "true";
@@ -20,12 +28,21 @@ export default function CustomHeader({ title }: { title: string }) {
         }}
       >
         {/* Conditionally render the back button */}
-        {!isFrom404 && (
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Text style={{ fontSize: 16, color: "blue" }}>Back</Text>
+        {canGoBack && !isFrom404 && (
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={{ marginInlineEnd: 25 }}
+            hitSlop={10}
+          >
+            {/* <Text style={{ fontSize: 16, color: "blue" }}>Back</Text> */}
+            <IconSymbol
+              name="arrowkeys.left.filled"
+              size={24}
+              color={"black"}
+            />
           </TouchableOpacity>
         )}
-        <Text style={{ fontSize: 20, fontWeight: 500, marginLeft: 16 }}>
+        <Text style={{ fontSize: 20, fontWeight: 500, marginLeft: 20 }}>
           {title}
         </Text>
       </View>
